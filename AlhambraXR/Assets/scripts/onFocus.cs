2a1d46aa8a5e3ca6906aeae775a2d337a7beca8f
Juan Carlos Torres <jctorres@ugr.es>
2022-07-29 13:54:53 +0200

    onfocus corrected. Point moving finger.

--- AlhambraXR/Assets/scripts/onFocus.cs
+++ AlhambraXR/Assets/scripts/onFocus.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 using Microsoft.MixedReality.Toolkit.Input;
 
-public class onFocus : MonoBehaviour , IMixedRealityFocusHandler, IMixedRealityFocusChangedHandler //IMixedRealityPointerHandler,
+public class onFocus : MonoBehaviour , IMixedRealityFocusHandler, IMixedRealityFocusChangedHandler //, IMixedRealityInputHandler //IMixedRealityPointerHandler,
 {
     GameObject Modelo;
     public Texture2D indexTexture;
@@ -40,8 +40,70 @@ public class onFocus : MonoBehaviour , IMixedRealityFocusHandler, IMixedRealityF
 
     //public UnityEngine.Ray Ray { get; }
     // public class HandRay : Microsoft.MixedReality.Toolkit.Input.IHandRay
+/*
+    public void OnInputDown(InputEventData eventData)
+    {
+        var result = eventData.Pointer.Result;
+        var spawnPosition = result.Details.Point; // PointLocalSpace
+        //    var spawnRotation = Quaternion.LookRotation(result.Details.Normal);
+
+        Focus.transform.position = spawnPosition;
+        Debug.Log("OnFocusEnter at " + spawnPosition);
+        Ray ray = new Ray(spawnPosition, -result.Details.Normal); //  Camera.main.ScreenPointToRay(spawnPosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == "CuartoDoradoSTa")
+            {
+                Vector2 punto = hit.textureCoord;
+                Debug.Log("u " + punto.x + " v " + punto.y);
+                c = indexTexture.GetPixel(Mathf.FloorToInt(punto.x * indexTexture.width), Mathf.FloorToInt(punto.y * indexTexture.height));
+                ir = Mathf.RoundToInt(c.r * 255);
+                ig = Mathf.RoundToInt(c.g * 255);
+                ib = Mathf.RoundToInt(c.b * 255);
+                ia = Mathf.RoundToInt(c.a * 255);
+                Debug.Log("OnFocus ir " + ir + " ig " + ig + " ib " + ib + " ia " + ia);
+
+                rend.material.shader = Shader.Find("Custom/BlinkSurface");
+
+                //    gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_ID", ir);
+
+                if (ir > 0)
+                {
+                    gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_ID", ir);
+                    gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Layer", 0);
+                }
+                else if (ig > 0)
+                {
+                    gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_ID", ig);
+                    gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Layer", 1);
+                }
+                else if (ib > 0)
+                {
+                    gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_ID", ib);
+                    gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Layer", 2);
+                }
+                else gameObject.GetComponent<Renderer>().sharedMaterial.SetFloat("_Layer", 4);
+
+                /*             if (id == 120)
+                            {
+                                Modelo.GetComponent<Renderer>().material.SetTexture("_DecalTex", tex12);
+                            }
+                             else
+                             {
+                                 Modelo.GetComponent<Renderer>().material.SetTexture("_DecalTex", null);
+                             }
+                         }* /
 
 
+            }
+        }
+
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    { }
+*/
     void IMixedRealityFocusHandler.OnFocusEnter(FocusEventData eventData)
     {
         //   if (Input.GetMouseButtonDown(0))
